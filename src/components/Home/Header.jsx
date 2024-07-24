ec3ee45c05ed9e58d0e208c87779d712614957e1
JhonLlapapasca <jhonllapapasca@gmail.com>
2024-07-24 15:24:45 -0500

    replace image diagram

---
+++ src/components/Home/Header.jsx
@@ -0,0 +1,62 @@
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import { MdControlPoint } from "react-icons/md";
+import Buttons from "../Buttons";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+
+
+export default function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+
+  const FeatureList = [
+    {
+      title: "Automatización de tareas",
+      // Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
+      description: (
+        <>
+          Programa y establece tareas repetitivas para su ejecucion, Tareas con
+          el formato <strong>cron</strong>, envio de correos electronicos con
+          previa configuracion.
+        </>
+      ),
+    },
+    {
+      title: "Registro de las ejecuciones",
+      // Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
+      description: (
+        <>
+          Facilitamos el registro de ejecuciones para tener mayores detalles de
+          las ejecuciones, errores y exitos que sucedan al momento de la
+          ejecucion.{" "}
+        </>
+      ),
+    },
+  ];
+  return (
+    <header className="text-start">
+      <div className="grid grid-cols-2 gap-4">
+        <div className="p-10 my-auto">
+          <div className="mb-10">
+            <h1 className="text-6xl font-bold my-5">{siteConfig.title}</h1>
+            <p className="text-2xl text-gray-500">{siteConfig.tagline}</p>
+          </div>
+          <Buttons />
+          <ul className="mt-10">
+            {FeatureList.map((props, idx) => (
+              <li key={idx}>
+                <h3 className="font-bold flex gap-1 items-center mt-5 mb-3 text-2xl">
+                  <MdControlPoint />
+                  {props.title}
+                </h3>
+                <p className="text-xl">{props.description}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div>
+          <img src={useBaseUrl("img/canvas.png")} alt="canvas" />
+        </div>
+      </div>
+    </header>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,69 +1,11 @@
-import useBaseUrl from "@docusaurus/useBaseUrl";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 
 import CustomSection from "../components/Section/CustomSection";
-import { MdControlPoint } from "react-icons/md";
 import Dashboard from "../components/Home/Dashboard";
-import Buttons from "../components/Buttons";
 import Diagrama from "../components/Home/Diagrama";
+import HomepageHeader from "../components/Home/Header";
 
-const FeatureList = [
-  {
-    title: "Automatización de tareas",
-    // Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
-    description: (
-      <>
-        Programa y establece tareas repetitivas para su ejecucion, Tareas con el
-        formato <strong>cron</strong>, envio de correos electronicos con previa
-        configuracion.
-      </>
-    ),
-  },
-  {
-    title: "Registro de las ejecuciones",
-    // Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
-    description: (
-      <>
-        Facilitamos el registro de ejecuciones para tener mayores detalles de
-        las ejecuciones, errores y exitos que sucedan al momento de la
-        ejecucion.{" "}
-      </>
-    ),
-  },
-];
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className="text-start">
-      <div className="grid grid-cols-2 gap-4">
-        <div className="p-10 my-auto">
-          <div className="mb-10">
-            <h1 className="text-6xl font-bold my-5">{siteConfig.title}</h1>
-            <p className="text-2xl text-gray-500">{siteConfig.tagline}</p>
-          </div>
-          <Buttons />
-          <ul className="mt-10">
-            {FeatureList.map((props, idx) => (
-              <li key={idx}>
-                <h3 className="font-bold flex gap-1 items-center mt-5 mb-3 text-2xl">
-                  <MdControlPoint />
-                  {props.title}
-                </h3>
-                <p className="text-xl">{props.description}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <div>
-          <img src={useBaseUrl("img/canvas.png")} alt="canvas" />
-        </div>
-      </div>
-    </header>
-  );
-}
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
